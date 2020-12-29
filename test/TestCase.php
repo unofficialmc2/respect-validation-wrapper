6ae7f239faa414bcb8f011121e2c4168becbda29
Fabien Sanchez <fzed51+git@gmail.com>
2020-12-29 09:31:02 +0100

    feat | mise en place des tests

---
+++ test/TestCase.php
@@ -0,0 +1,13 @@
+<?php
+namespace Respect\Validaton\Wrapper\Test;
+
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+
+/**
+ * Class TestCase de base pour les tests du projet
+ * @package Respect\Validato\Wrapper\Test
+ */
+class TestCase extends PHPUnitTestCase
+{
+
+}
